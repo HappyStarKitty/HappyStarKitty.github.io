826c3da0c46491b0aad4fa6f4c0aaba3b5076bbc
happystarkitty <2284135650@qq.com>
2024-06-30 15:48:13 +0800

    Site updated: 2024-06-30 15:48:12

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1719730785628";
+const VERSION = "1719733691567";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
