2b7cfc0557cb463602dedc98d8df96acec913ad5
DakiMoon <2284135650@qq.com>
2024-10-29 10:12:56 +0800

    Site updated: 2024-10-29 10:12:53

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1729905222587";
+const VERSION = "1730167919636";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
