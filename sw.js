a397e55390ce13425dcf26b2bc20a2836c0de577
DakiMoon <2284135650@qq.com>
2026-01-09 21:21:36 +0800

    Site updated: 2026-01-09 21:21:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767422962299";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1767964882458";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
